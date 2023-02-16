2e466ddbbc282bf86b34336c3f2b2ae17368d95c
khansazulfa9 <khansazulfa9@gmail.com>
2023-02-16 15:35:37 +0700

    Update

--- src/App.svelte
+++ src/App.svelte
@@ -2,14 +2,22 @@
   import Navbar from "./Navbar/Navbar.svelte";
   import Table from "./Table/Table.svelte";
   import TambahKeranjang from "./TambahKeranjang/TambahKeranjang.svelte";
-  import { Col, Container, Row } from 'sveltestrap';
+  import { Container, ListGroup } from 'sveltestrap';
+  import List from "./List/List.svelte";
 
 </script>
 
 <Navbar/>
 <br>
 <Container>
+  
 <Table/> 
+<!-- <ListGroup>
+  <List produk = {'Nivea'} desc = {'Hand Body'}/>
+  <List produk = {'Vaseline'}/>
+  <List/>
+  <List/> 
+</ListGroup> -->
 <TambahKeranjang/>
 </Container>
 <style>
