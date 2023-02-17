3220c6ef536c59ff7efb4e4b9a6e89ea490796c6
khansazulfa9 <khansazulfa9@gmail.com>
2023-02-17 15:08:11 +0700

    function-if else

--- src/TambahKeranjang/TambahKeranjang.svelte
+++ src/TambahKeranjang/TambahKeranjang.svelte
@@ -1,6 +1,15 @@
 <script>
 	import { Col, Container, Row, Button } from 'sveltestrap';
 	
+	let keranjang  = 0
+	let harga = 100000
+
+	$: hargatotal = keranjang * harga
+
+	function tambahKeranjang(){
+		keranjang +=1
+	}
+
 	let box = 'blue'
 	function setcolor(){
 		box=='blue'?box='red':box='blue'
@@ -8,6 +17,16 @@
 </script>
 
 <Container>
+	<Row>
+	<Col>
+		<br>
+		<Button on:click={tambahKeranjang}>Add to chart</Button>
+	</Col>
+	<Col>
+		<br>
+		Jumlah Keranjang: {keranjang} <br> 
+		Harga Total: Rp {hargatotal}</Col>
+	<Col>
 	<br>
 	<button on:click={setcolor}>Change color</button>
 	{#if box=='red'}
@@ -15,4 +34,6 @@
 		{:else}
 		<h2 style="background-color: blue ;">Iam Blue</h2>
 	{/if}
+	</Col>
+	</Row>
 </Container>
