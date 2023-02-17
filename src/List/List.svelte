57c140721646c74e8ae25b279176420d8b1409ae
khansazulfa9 <khansazulfa9@gmail.com>
2023-02-17 14:27:48 +0700

    properti

---
+++ src/List/List.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { Row, Col, ListGroupItem } from "sveltestrap";
+//Props
+  export let produk = "Tidak ada merk";
+  export let desc = "Tidak ada deskripsi";
+
+</script>
+
+<ListGroupItem>
+    <Row>
+        <Col>
+            <img src="image/foto-produk.png" alt="foto produk" width="100"/>
+        </Col>
+        <Col>
+            <h5>{produk}</h5>
+            <p>{desc}</p>
+        </Col>
+    </Row>
+</ListGroupItem>
