677c0db32c535b827d198d0f0f9e7ca87b3c767f
khansazulfa9 <khansazulfa9@gmail.com>
2023-03-07 15:35:59 +0700

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,14 @@
 <script>
-  
+	import { todos } from "../stores/formStore";
+    import TodoForm from "../components/TodoForm.svelte";
+    import Todo from "../components/Todo.svelte";
 </script>
 
 <main>
-    <h1 class="text-2xl font-bold text-center text-gray-800 md:text-1xl mt-3">Tes dulu</h1>
+    <h1 class="text-2xl font-bold text-center text-gray-800 md:text-1xl mt-3">Form</h1>
+    <TodoForm/>
 
-<form action="" class="my-6"> 
-    <div class="flex flex-col text-sm mb-2 ml-5 mr-5">
-        <label class="font-bold mb-2 text-gray-800 my-3" for="name">Nama</label>
-        <input type="text" name="nama" placeholder="tulis disini" class=" px-5 py-2 border">
-         <label class="font-bold mb-2 text-gray-800 my-3" for="name">Email</label>
-        <input type="text" name="nama" placeholder="tulis disini" class=" px-5 py-2 border">
-    </div> 
-        <button type="submit" class=" shadow-lg px-5 py-2 my-2 shadow-cyan-500/50 ml-5 bg-sky-700 rounded ">Submit</button>
-    
-</form>
- 
+{#each $todos as todo}
+    <Todo todo={todo} index={todo.id}/>
+ {/each}
 </main>
