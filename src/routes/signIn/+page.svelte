ed0a568b0e1fec4c24773b26ec18a8aeb4aac67b
khansazulfa9 <khansazulfa9@gmail.com>
2023-03-20 15:51:00 +0700

    Page-Sign In

---
+++ src/routes/signIn/+page.svelte
@@ -0,0 +1,23 @@
+<section class="body-font">
+    <div class="container px-5 py-24 mx-auto flex flex-wrap items-center">
+        <div class="lg:w-2/6 md:w-1/2 bg-opacity-50 rounded-lg border p-8 flex flex-col md:mx-auto w-full  md:mt-0">
+            <h1 class=" text-lg font-bold mx-auto title-font mb-5">Sign In</h1>
+            <div class="relative mb-4">
+                <label for="email" class="leading-7 text-sm text-gray-500">Email</label>
+                <input type="text" id="email" name="email" class="w-full bg-gray-400 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-lime-600 rounded border focus:border-lime-700 text-base outline-none  py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+            </div>
+            <div class="relative mb-4">
+                <label for="password" class="leading-7 text-sm text-gray-500">Password</label>
+                <input type="password" id="password" name="password" class="w-full bg-gray-400 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-lime-600 rounded border  focus:border-lime-700 text-base outline-none py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+            </div>
+            <div class="mb-3 form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <label class="form-check-label text-xs" for="exampleCheck1">Tetap Masuk</label>
+            </div>
+            <button class="text-white bg-lime-700 border-0 py-2 px-8 focus:outline-none hover:bg-lime-800 rounded text-md">Sign In</button>
+            
+            <p class="text-xs mt-3"><a href="/"> Lupa Password?</a></p>
+            <button class="text-white text-sm bg-lime-500 border-0 py-2 px-8 mt-24 focus:outline-none rounded">Belum memiliki akun? <a href="/" class="text-lime-900">Daftar Sekarang</a></button>
+        </div>
+    </div>
+</section>
